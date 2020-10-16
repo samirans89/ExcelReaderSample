b0b2c343daf53f65402d3317541b2d1f0423e04d
Samiran Saha <samiran.s89@gmail.com>
2020-10-16 18:10:22 +0530

    init commit excel reader sampler

---
+++ ExcelReader/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace ExcelReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            Console.WriteLine(path);
+            XSSFWorkbook xssfwb;
+
+            using (FileStream file = new FileStream(@"TestExcelReader.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                xssfwb = new XSSFWorkbook(file);
+            }
+
+            ISheet sheet = xssfwb.GetSheet("Sheet1");
+            for (int row = 0; row <= sheet.LastRowNum; row++)
+            {     
+                for (int col = 0; col < sheet.GetRow(row).LastCellNum; col++)
+                {
+                    if (sheet.GetRow(row) != null) //null is when the row only contains empty cells 
+                    {
+                        if (sheet.GetRow(row).GetCell(col).CellType.Equals(CellType.String))
+                        {
+                            Console.Write(string.Format("{0}\t", sheet.GetRow(row).GetCell(col).StringCellValue));
+                        }
+                        else
+                        {
+                            Console.Write(string.Format("{0}\t", sheet.GetRow(row).GetCell(col).NumericCellValue));
+                        } 
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+
+            /*
+            var fileName = @"/Users/samiran/Projects/ExcelReader/ExcelReader/TestExcelReader.xlsx";
+            var connectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + fileName + ";Extended Properties=\"Excel 12.0;IMEX=1;HDR=NO;TypeGuessRows=0;ImportMixedTypes=Text\""; ;
+            using (var conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+
+                var sheets = conn.GetOleDbSchemaTable(System.Data.OleDb.OleDbSchemaGuid.Tables, new object[] { null, null, null, "TABLE" });
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM [" + sheets.Rows[0]["TABLE_NAME"].ToString() + "] ";
+
+                    var adapter = new OleDbDataAdapter(cmd);
+                    var ds = new DataSet();
+                    adapter.Fill(ds);
+                }
+            }
+            */
+        }
+    }
+}
